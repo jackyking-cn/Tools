95878293a98986f78d96ddb7594505ecfdd6cfd4
jackyking-cn <jackyking.cn@gmail.com>
2020-06-18 17:24:16 +0800

    feat: support regex for each row.

--- RegularTester/script/script.js
+++ RegularTester/script/script.js
@@ -1,26 +1,46 @@
 function $(id) {
-    return document.getElementById(id);
+  return document.getElementById(id);
 }
 
 function matchString() {
-    var v = $('source').value,
-        r = $('regular').value;
-    
-    if (!/^\//.test(r)) {
-        r = '/' + r + '/';
+  var v = $("source").value,
+    r = $("regular").value;
+
+  if (!/^\//.test(r)) {
+    r = "/" + r + "/";
+  }
+
+  if ($("row").checked) {
+    var a = v.split(/\r?\n/);
+
+    for (var i = 0; i < a.length; i++) {
+      a[i] = a[i].match(eval(r));
     }
-    
-    $('result').value = v.match(eval(r));
+
+    $("result").value = a.join("\n");
+  } else {
+    $("result").value = v.match(eval(r));
+  }
 }
 
 function replaceString() {
-    var v = $('source').value,
-        r = $('regular').value,
-        s = $('replace').value;
-    
-    if (!/^\//.test(r)) {
-        r = '/' + r + '/';
+  var v = $("source").value,
+    r = $("regular").value,
+    s = $("replace").value;
+
+  if (!/^\//.test(r)) {
+    r = "/" + r + "/";
+  }
+
+  if ($("row").checked) {
+    var a = v.split(/\r?\n/);
+
+    for (var i = 0; i < a.length; i++) {
+      a[i] = a[i].replace(eval(r), s);
     }
-    
-    $('result').value = v.replace(eval(r), s);
+
+    $("result").value = a.join("\n");
+  } else {
+    $("result").value = v.replace(eval(r), s);
+  }
 }
